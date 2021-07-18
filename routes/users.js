14e622580edf2ab7efdd421b4161c8f6dd1e67d1
Vinay Nation <vinay.raj.apple@gmail.com>
2021-07-18 18:35:51 +0530

    Added find and Delete request

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router(); // Initialising router here 
 
 // all routes in here are staring with /users 
 
-var users = []
+let users = [];
 
 router.get('/', (req, res) => {
       console.log(users);
@@ -13,8 +14,22 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const user = req.body;
-    users.push(user);
+    const userDataWithId = { ...user, id: uuidv4() }
+    users.push(userDataWithId);
     res.send(`User with the name ${user.firstName} added to the database`);
+});
+
+router.get('/:id', (req, res) => {
+     const { id } = req.params; // get id values from params
+     const foundUser = users.find((user) => user.id == id);
+    res.send(foundUser);
+})
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id != id);
+    res.send(`User with the id ${id} deleted from the database`);
 });
 
 export default router;
